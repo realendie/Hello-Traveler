b7177831713a1a835edb10b09923c8597bb90ca4
Wyatt Johnson <116504461+realendie@users.noreply.github.com>
2024-12-15 02:50:19 -0500

    Update main.py
    See v0.2 changelog.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from gooey import Gooey, GooeyParser
 import subprocess
 import sys
-
+import time
 
 start = input('Old Man: yawn.... Oh hello there traveler! Would you like to play or die?\n')
 
@@ -12,10 +12,12 @@ if start.lower() == 'play':
     print('Old Man: Wonderful lets play. What is your name?\n')
 elif start.lower() == 'die':
     print('Lame, well, you die now.(Ending 1/6)')
-    
+    time.sleep(20)
+    quit(code=1)
 else:
     print('invalid response (Ending 0/6)')
-    
+    time.sleep(10)
+    quit(code=0)
 
 name = input()
 setting = input(f'Old Man: Hello, {name} you like to go the attic or the basement?\n')
@@ -26,10 +28,12 @@ if setting.lower() == 'attic':
     box = input('Do you open the box or leave it to go explore?\n')
 elif setting.lower() == 'basement':
     print('You fall down the stairs to the basement, hit your head and die. (Ending 2/6)')
-    
+    time.sleep(20)
+    quit(code=2)
 else:
     print('invalid response (Ending 0/6)')
-    
+    time.sleep(10)
+    quit(code=0)
 
 # Box handling - normalized to lowercase for user input
 if box.lower() == 'explore' or box.lower() == 'leave it':
@@ -37,10 +41,12 @@ if box.lower() == 'explore' or box.lower() == 'leave it':
     light = input('You see two light switches, do you flick the left or the right one?\n')
 elif box.lower() == 'open it' or box.lower() == 'open':
     print('You open the box, the old man then pushes you in and you die of suffocation. (Ending 3/6)')
-    
+    time.sleep(20)
+    quit(code=3)
 else:
     print('invalid response (Ending 0/6)')
-    
+    time.sleep(10)
+    quit(code=0)
 
 # Light switch handling - normalized to lowercase
 if light.lower() == 'right':
@@ -49,7 +55,8 @@ elif light.lower() == 'left':
     print('The light turns on... you see a dark shadowy figure in the corner')
 else:
     print('invalid response (Ending 0/6)')
-    
+    time.sleep(10)
+    quit(code=0)
 
 # Attic corner figure handling - normalized to lowercase
 figure = input('Do you approach it?\n')
@@ -63,15 +70,19 @@ if figure.lower() == 'yes':
         print('"You have found me", the voice whispers, "I have been waiting for you".')
         print('You walk towards the figure. You reach out and your fingers brush against the fabric. The shadow seems to grow.')
         print('You get pulled into the fabric and the shadow engulfs you. You feel a sharp pain, then everything goes black. (Ending 4/6)')
-       
+        time.sleep(20)
+        quit(code=4)
     else:
         print('invalid response (Ending 0/6)')
-     
+        time.sleep(10)
+        quit(code=0)
+
 elif figure.lower() == 'no':
     print('Old Man: We should probably head downstairs now.')
 else:
     print('invalid response (Ending 0/6)')
-    
+    time.sleep(10)
+    quit(code=0)
 
 # Downstairs again handling - normalized to lowercase
 tea = input('Would you like some tea?')
@@ -83,13 +94,15 @@ if tea.lower() == 'yes':
     if wait.lower() == 'wait':
         print('You wait 5 more minutes and he arrives with your tea.')
         print('You drink the entire mug of tea hastily. Everything then fades to black, you\'ve been poisoned. (Ending 5/6)')
-        
+        quit(code=5)
     elif wait.lower() == 'leave':
         print('You leave the house and hitchhike back to town.(Ending 6/6)')
     else:
         print('invalid response (Ending 0/6)')
-        
+        time.sleep(10)
+        quit(code=0)
 
 # Final outcome
 print('You Survived!')
 print('Thank you for playing. I hope you enjoyed!\n-endie')
+quit(code=6)
